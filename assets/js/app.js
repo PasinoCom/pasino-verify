198f1f9fb8be6920b838d4c8203acc77c91619c5
PasinoCom <79631710+PasinoCom@users.noreply.github.com>
2021-02-25 09:42:50 +0100

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,599 @@
+var plinko_payouts = {
+    "8": {
+        "low": [
+            "5.6x",
+            "2.1x",
+            "1.1x",
+            "1x",
+            "0.5x",
+            "1x",
+            "1.1x",
+            "2.1x",
+            "5.6x"
+        ],
+        "medium": [
+            "13x",
+            "3x",
+            "1.3x",
+            "0.7x",
+            "0.4x",
+            "0.7x",
+            "1.3x",
+            "3x",
+            "13x"
+        ],
+        "high": [
+            "29x",
+            "4x",
+            "1.5x",
+            "0.3x",
+            "0.2x",
+            "0.3x",
+            "1.5x",
+            "4x",
+            "29x"
+        ]
+    },
+    "9": {
+        "low": [
+            "5.6x",
+            "2x",
+            "1.6x",
+            "1x",
+            "0.7x",
+            "0.7x",
+            "1x",
+            "1.6x",
+            "2x",
+            "5.6x"
+        ],
+        "medium": [
+            "18x",
+            "4x",
+            "1.7x",
+            "0.9x",
+            "0.5x",
+            "0.5x",
+            "0.9x",
+            "1.7x",
+            "4x",
+            "18x"
+        ],
+        "high": [
+            "43x",
+            "7x",
+            "2x",
+            "0.6x",
+            "0.2x",
+            "0.2x",
+            "0.6x",
+            "2x",
+            "7x",
+            "43x"
+        ]
+    },
+    "10": {
+        "low": [
+            "8.9x",
+            "3x",
+            "1.4x",
+            "1.1x",
+            "1x",
+            "0.5x",
+            "1x",
+            "1.1x",
+            "1.4x",
+            "3x",
+            "8.9x"
+        ],
+        "medium": [
+            "22x",
+            "5x",
+            "2x",
+            "1.4x",
+            "0.6x",
+            "0.4x",
+            "0.6x",
+            "0.4x",
+            "0.6x",
+            "1.4x",
+            "2x",
+            "5x",
+            "22x"
+        ],
+        "high": [
+            "76x",
+            "10x",
+            "3x",
+            "0.9x",
+            "0.3x",
+            "0.2x",
+            "0.3x",
+            "0.9x",
+            "3x",
+            "10x",
+            "76x"
+        ]
+    },
+    "11": {
+        "low": [
+            "8.4x",
+            "3x",
+            "1.9x",
+            "1.3x",
+            "1x",
+            "0.7x",
+            "0.7x",
+            "1x",
+            "1.3x",
+            "1.9x",
+            "3x",
+            "8.4x"
+        ],
+        "medium": [
+            "24x",
+            "6x",
+            "3x",
+            "1.8x",
+            "0.7x",
+            "0.5x",
+            "0.5x",
+            "0.7x",
+            "1.8x",
+            "3x",
+            "6x",
+            "24x"
+        ],
+        "high": [
+            "120x",
+            "14x",
+            "5.2x",
+            "1.4x",
+            "0.4x",
+            "0.2x",
+            "0.2x",
+            "0.4x",
+            "1.4x",
+            "5.2x",
+            "14x",
+            "120x"
+        ]
+    },
+    "12": {
+        "low": [
+            "10x",
+            "3x",
+            "1.6x",
+            "1.4x",
+            "1.1x",
+            "1x",
+            "0.5x",
+            "1x",
+            "1.1x",
+            "1.4x",
+            "1.6x",
+            "3x",
+            "10x"
+        ],
+        "medium": [
+            "33x",
+            "11x",
+            "4x",
+            "2x",
+            "1.1x",
+            "0.6x",
+            "0.3x",
+            "0.6x",
+            "1.1x",
+            "2x",
+            "4x",
+            "11x",
+            "33x"
+        ],
+        "high": [
+            "170x",
+            "24x",
+            "8.1x",
+            "2x",
+            "0.7x",
+            "0.2x",
+            "0.2x",
+            "0.2x",
+            "0.7x",
+            "2x",
+            "8.1x",
+            "24x",
+            "170x"
+        ]
+    },
+    "13": {
+        "low": [
+            "8.1x",
+            "4x",
+            "3x",
+            "1.9x",
+            "1.2x",
+            "0.9x",
+            "0.7x",
+            "0.7x",
+            "0.9x",
+            "1.2x",
+            "1.9x",
+            "3x",
+            "4x",
+            "8.1x"
+        ],
+        "medium": [
+            "43x",
+            "13x",
+            "6x",
+            "3x",
+            "1.3x",
+            "0.7x",
+            "0.4x",
+            "0.4x",
+            "0.7x",
+            "1.3x",
+            "3x",
+            "6x",
+            "13x",
+            "43x"
+        ],
+        "high": [
+            "260x",
+            "37x",
+            "11x",
+            "4x",
+            "1x",
+            "0.2x",
+            "0.2x",
+            "0.2x",
+            "0.2x",
+            "1x",
+            "4x",
+            "11x",
+            "37x",
+            "260x"
+        ]
+    },
+    "14": {
+        "low": [
+            "7.1x",
+            "4x",
+            "1.9x",
+            "1.4x",
+            "1.3x",
+            "1.1x",
+            "1x",
+            "0.5x",
+            "1x",
+            "1.1x",
+            "1.3x",
+            "1.4x",
+            "1.9x",
+            "4x",
+            "7.1x"
+        ],
+        "medium": [
+            "58x",
+            "15x",
+            "7x",
+            "4x",
+            "1.9x",
+            "1x",
+            "0.5x",
+            "0.2x",
+            "0.5x",
+            "1x",
+            "1.9x",
+            "4x",
+            "7x",
+            "15x",
+            "58x"
+        ],
+        "high": [
+            "420x",
+            "59x",
+            "18x",
+            "5x",
+            "1.9x",
+            "0.3x",
+            "0.2x",
+            "0.2x",
+            "0.2x",
+            "0.3x",
+            "1.9x",
+            "5x",
+            "18x",
+            "56x",
+            "420x"
+        ]
+    },
+    "15": {
+        "low": [
+            "15x",
+            "8x",
+            "3x",
+            "2x",
+            "1.5x",
+            "1.1x",
+            "1x",
+            "0.7x",
+            "0.7x",
+            "1x",
+            "0.1x",
+            "1.5x",
+            "2x",
+            "3x",
+            "8x",
+            "15x"
+        ],
+        "medium": [
+            "88x",
+            "18x",
+            "11x",
+            "5x",
+            "3x",
+            "1.3x",
+            "0.5x",
+            "0.3x",
+            "0.3x",
+            "0.5x",
+            "1.3x",
+            "3x",
+            "5x",
+            "11x",
+            "18x",
+            "88x"
+        ],
+        "high": [
+            "620x",
+            "83x",
+            "27x",
+            "8x",
+            "3x",
+            "0.5x",
+            "0.2x",
+            "0.2x",
+            "0.2x",
+            "0.2x",
+            "0.5x",
+            "3x",
+            "8x",
+            "27x",
+            "83x",
+            "620x"
+        ]
+    },
+    "16": {
+        "low": [
+            "16x",
+            "9x",
+            "2x",
+            "1.4x",
+            "1.4x",
+            "1.2x",
+            "1.1x",
+            "1x",
+            "0.5x",
+            "1x",
+            "1.1x",
+            "1.2x",
+            "1.4x",
+            "1.4x",
+            "2x",
+            "9x",
+            "16x"
+        ],
+        "medium": [
+            "110x",
+            "41x",
+            "10x",
+            "5x",
+            "3x",
+            "1.5x",
+            "1x",
+            "0.5x",
+            "0.3x",
+            "0.5x",
+            "1x",
+            "1.5x",
+            "3x",
+            "5x",
+            "10x",
+            "41x",
+            "110x"
+        ],
+        "high": [
+            "1000x",
+            "130x",
+            "26x",
+            "9x",
+            "4x",
+            "2x",
+            "0.2x",
+            "0.2x",
+            "0.2x",
+            "0.2x",
+            "0.2x",
+            "2x",
+            "4x",
+            "9x",
+            "26x",
+            "130x",
+            "1000x"
+        ]
+    }
+};
+
+function calculateDiceResult() {
+
+    var client_seed = $("#client_seed").val();
+    var server_seed = $("#server_seed").val();
+    var nonce = $("#nonce").val();
+
+    var combo = server_seed + ":" + client_seed + ":" + nonce;
+
+    var md = forge.md.sha256.create();
+    md.update(server_seed);
+    var server_seed_hash = md.digest().toHex();
+
+    var md = forge.md.sha512.create();
+    md.update(combo);
+    combo = md.digest().toHex();
+
+    var combo_length = combo.length;
+    var index = 0;
+    var lucky = 1000000;
+
+    while (lucky >= 1000000) {
+
+        hex_string = combo.substr(index, 5);
+        lucky = parseInt((hex_string + '').replace(/[^a-f0-9]/gi, ''), 16);
+
+        if ((index + 5) > combo_length) {
+            break;
+        }
+
+        index = index + 5;
+
+    }
+
+    if (lucky < 1000000) {
+        roll = lucky % 10000;
+        $('#result').val(roll);
+    } else {
+        $('#result').val("Impossible Game");
+    }
+
+    $("#server_seed_hash").val(server_seed_hash);
+
+}
+
+function calculatePlinkoResult() {
+
+    var client_seed = $("#client_seed").val();
+    var server_seed = $("#server_seed").val();
+    var nonce = $("#nonce").val();
+    var rows = $("#rows").val();
+    var risk = $("#risk").val();
+
+    var seeds = [];
+    var split_numbers = [];
+    var position = 0;
+
+    var md = forge.md.sha256.create();
+    md.update(server_seed);
+    var server_seed_hash = md.digest().toHex();
+
+    var series_1 = forge.hmac.create();
+    series_1.start('sha256', server_seed);
+    series_1.update(client_seed + ":" + nonce + ":0");
+    series_1 = series_1.digest().toHex();
+
+    var series_2 = forge.hmac.create();
+    series_2.start('sha256', server_seed);
+    series_2.update(client_seed + ":" + nonce + ":1");
+    series_2 = series_2.digest().toHex();
+
+    var series_3 = forge.hmac.create();
+    series_3.start('sha256', server_seed);
+    series_3.update(client_seed + ":" + nonce + ":2");
+    series_3 = series_3.digest().toHex();
+
+    let s = 0;
+
+    for (x = 0; x < 32; x++) {
+        s = x * 2;
+        seeds.push(series_1.substring(x * 2, s + 2));
+    }
+
+    for (x = 0; x < 32; x++) {
+        s = x * 2;
+        seeds.push(series_2.substring(x * 2, s + 2));
+    }
+
+    for (x = 0; x < 32; x++) {
+        s = x * 2;
+        seeds.push(series_3.substring(x * 2, s + 2));
+    }
+
+    for (x = 0; x < 20; x++) {
+
+        let s = x * 4;
+
+        let num1 = parseFloat(parseInt(seeds[s], 16) / Math.pow(256, 1)).toFixed(12);
+        let num2 = parseFloat(parseInt(seeds[s + 1], 16) / Math.pow(256, 2)).toFixed(12);
+        let num3 = parseFloat(parseInt(seeds[s + 2], 16) / Math.pow(256, 3)).toFixed(12);
+        let num4 = parseFloat(parseInt(seeds[s + 3], 16) / Math.pow(256, 4)).toFixed(12);
+
+        let sum = Math.floor((+num1 + +num2 + +num3 + +num4) * 2);
+
+        split_numbers.push(sum);
+
+    }
+
+    split_numbers = split_numbers.slice(0, rows);
+
+    for (i = 0; i < split_numbers.length; i++) {
+        position = +position + +split_numbers[i];
+    }
+
+    console.log(position);
+
+    $("#result").val(plinko_payouts[rows][risk][position]);
+    $("#server_seed_hash").val(server_seed_hash);
+
+}
+
+function toFixed(num, fixed) {
+    var re = new RegExp("^-?\\d+(?:\.\\d{0," + (fixed || -1) + "})?");
+    return num.toString().match(re)[0];
+}
+
+function calculateCrashResult() {
+
+    var client_seed = $("#client_seed").val();
+    var last_seed = $("#server_seed").val();
+    var games = $("#games").val();
+
+    var actual_hash;
+    var p1, p2, p3, p4, roll, crash_point;
+
+    $("#tabled > tbody").html("");
+
+    for (x = 0; x <= games; x++) {
+
+        var md = forge.md.sha512.create();
+        md.update(client_seed + "-" + last_seed);
+        actual_hash = md.digest().toHex();
+
+        p1 = parseInt((actual_hash.substr(0, 2) + "").replace(/[^a-f0-9]/gi, ""), 16);
+        p2 = parseInt((actual_hash.substr(2, 2) + "").replace(/[^a-f0-9]/gi, ""), 16);
+        p3 = parseInt((actual_hash.substr(4, 2) + "").replace(/[^a-f0-9]/gi, ""), 16);
+        p4 = parseInt((actual_hash.substr(6, 2) + "").replace(/[^a-f0-9]/gi, ""), 16);
+
+        roll = Math.floor((p1 / Math.pow(256, 1) + p2 / Math.pow(256, 2) + p3 / Math.pow(256, 3) + p4 / Math.pow(256, 4)) * 1000000);
+        crash_point = toFixed(parseFloat((1000000 / (roll + 1)) * 0.96), 2);
+
+        $("#tabled > tbody").append("<tr>" + "<td>" + last_seed + "</td>" + "<td>" + crash_point + "x</td>" + "</tr>");
+
+        var md = forge.md.sha256.create();
+        md.update(last_seed);
+        last_seed = md.digest().toHex();
+
+    }
+
+}
+
+$("#dice-calculate-result").click(function (e) {
+    calculateDiceResult();
+});
+
+$("#plinko-calculate-result").click(function (e) {
+    calculatePlinkoResult();
+});
+
+$("#crash-calculate-result").click(function (e) {
+    calculateCrashResult();
+});
